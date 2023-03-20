329ab3d1d6fc76c70f906bbb55a4f825306443f6
Matei Biciușcă <white.hollow.ro@gmail.com>
2023-03-20 14:07:36 +0200

    Lab4

--- app/Main.hs
+++ app/Main.hs
@@ -1,6 +1,24 @@
-module Main (main) where
+module Main where
 
-import Lib
+import System.IO
+import System.Console.Isocline
+
+import Exp
+import Parsing
+import Printing
+import REPLCommand
+import Text.ParserCombinators.Parsec(parse)
 
 main :: IO ()
-main = someFunc
+main = do
+    setHistory "history.txt" 200
+    input <- readline "miniHaskell"
+    case parse replCommand "<input>" input of
+      Left err -> print err >> main
+      Right cmd -> case cmd of
+                    Quit -> return ()
+                    Load s -> putStrLn "notImplemented" >> main
+                    Eval l -> case parse exprParser "<input>" l of
+                        Left err -> print err >> main
+                        Right c -> putStrLn ( showExp c ) >> main
+                        
